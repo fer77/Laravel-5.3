d7034d94319a5c20f9cfa2b098f373dde75aa6e7
Fernando Fernandez <ffmp777@yahoo.com>
2017-05-23 12:32:45 -0600

    created welcome route and  email

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome Email</title>
+</head>
+<body>
+	<h1>Welcome To The Site {{ $user->name }}</h1>
+	<p>Hi boys. I'm Tina. Put it on a t-shirt, Jimmy Jr, I don't want to hear it. I'm no hero. I just put my bra on one boob at a time. Crap attack? Don't have one? Jimmy Jr. had a chance to ride the Tina truck, but now it's headed straight for Joshville. Toot-toot. Suddenly, Tina Belcher appeared in the doorway. She knew what she had to do. This is such a snore-gasm. How do you know a python ate it? Did it leave a note? I thought I saw a baby bump.</p>
+</body>
+</html>
