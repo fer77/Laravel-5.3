42cf26d3ce72d023447a381f1e202dcc32ac2f8d
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2017-06-15 11:09:22 -0600

    clean installation

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,16 @@
 <?php
-Auth::loginUsingId(1);
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::post('avatars', function() {
-	//request()->file('avatar')->store('avatars');// specifies a folder name.
-	//if no unique file is needed then:
-	$file = request()->file('avatar');
-	$ext = $file->extension();// you can also do $file->extension()
-
-	$file->storeAs('avatars/' . auth()->id(), "avatar.{$ext}");// to make it jeneric and not over-write it everytime (don't forget the "" when using {$...}).
-
-	return back();
 });
